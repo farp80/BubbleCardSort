75334906282be249397cbe739fc9dc2f362e16d5
farp80 <farp80@gmail.com>
2019-07-19 00:08:31 +0000

    initialversion

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,30 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = function() {};
+
+let suit = () => {
+  let randomSuit = Math.floor(Math.random() * 4);
+  let existingSuits = ["&;", "&;", "&;", "&;"];
+};
+
+let card = () => {
+  let randomCard = Math.floor(Math.random() * 12);
+  let existingCards = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  return existingCards[randomCard];
 };
